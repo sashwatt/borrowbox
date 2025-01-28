a1c1c8aa5f0830f3ab52b0c4a522697568b9a815
sashwatt <supersashwat@gmail.com>
2025-01-28 20:39:14 +0545

    splash cubit error fixed

--- tech_rental/lib/features/splash/presentation/view_model/splash_cubit.dart
+++ tech_rental/lib/features/splash/presentation/view_model/splash_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tech_rental/features/auth/presentation/view/login_page.dart';
+import 'package:tech_rental/app/view/onboard_screen.dart';
 import 'package:tech_rental/features/auth/presentation/view_model/login/login_bloc.dart';
 
 class SplashCubit extends Cubit<void> {
@@ -9,17 +9,14 @@ class SplashCubit extends Cubit<void> {
   final LoginBloc _loginBloc;
 
   Future<void> init(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 2), () async {
+    await Future.delayed(const Duration(seconds: 3), () async {
       // Open Login page or Onboarding Screen
 
       if (context.mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => BlocProvider.value(
-              value: _loginBloc,
-              child: LoginPage(),
-            ),
+            builder: (context) => const OnboardScreen(),
           ),
         );
       }
