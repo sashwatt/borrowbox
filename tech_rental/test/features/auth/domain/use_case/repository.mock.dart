85562498c7091cc9506d1395f6fce98619db9a3f
sashwatt <supersashwat@gmail.com>
2025-02-07 15:11:21 +0545

    mocktail and .mock files created

---
+++ tech_rental/test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tech_rental/features/auth/domain/repository/auth_repository.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
---
+++ tech_rental/test/features/auth/domain/use_case/token.mock.dart
@@ -0,0 +1,3 @@
+// import 'package:mocktail/mocktail.dart';
+
+// class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
